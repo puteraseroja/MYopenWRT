ed4ddb377fc59df176f56b6428c9946adb1ca641
ajoresaretup <88308937+puteraseroja@users.noreply.github.com>
2022-12-09 18:36:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,6 +10,29 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+# Enable wifi
+sed -i 's/set wireless.radio${devidx}.disabled=1/set wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Change SSID
+sed -i "s/set wireless.default_radio\${devidx}.ssid=\(^.*\)/wireless.default_radio\${devidx}.ssid=PuteraSeroja\${mode_band}/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Change hostname
+sed -i "s/system.@system[-1].hostname='OpenWrt'/set system.@system[-1].hostname='PuteraSeroja'/g" package/base-files/files/bin/config_generate
+
+# Set Description and Notes
+#echo "system.@system[-1].description='compiled by puteraseroja'"
+#echo "system.@system[-1].notes='this firmware is free. download  at telegram channel: t.me/+3j15vqnEPihmNWQ1'/g" >>package/base-files/files/bin/config_generate
+
+# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
+#sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
+
+# Add the default password for the 'root' user（Change the empty password to 'password'）
+#sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# Set etc/openwrt_release
+#sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+#echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_release
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
